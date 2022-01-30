6de518d5b40651e58fd7a064872904d293ac194b
Justin Desrosier <jbdesrosier@gmail.com>
2022-01-30 09:38:49 -0400

    add osbornesample

--- script1.py
+++ script1.py
@@ -10,5 +10,9 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/osbornesample")
+def osbornesample():
+    return render_template("osbornesample.html")
+
 if __name__=="__main__":
     app.run(debug=True)
