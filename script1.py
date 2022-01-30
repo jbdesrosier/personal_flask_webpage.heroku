94e72a51b772d027cacc507400ad9656c8ed61b8
Justin Desrosier <jbdesrosier@gmail.com>
2022-01-30 19:42:19 -0400

    major overhaul2

--- script1.py
+++ script1.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, url_for
+#, resized_img_src
+import os
+import flask_resize
 
 app=Flask(__name__)
+
+app.config['RESIZE_URL'] = 'jbdesrosier.info/'
+app.config['RESIZE_ROOT'] = 'C:/Users/jbdes/Desktop/Flask/personal_site/'
+
+resize = flask_resize.Resize(app)
+
+#app.secret_key='giraffe'
+#images=Images(app)
 
 @app.route("/")
 def home():
@@ -13,6 +24,9 @@ def contactInfo():
 @app.route("/CV")
 def CV():
     return render_template("cv.html")
+#    workingdir = os.path.abspath(os.getcwd())
+#    filepath = workingdir + '/static/cv/'
+#    return send_from_directory(filepath, "NBIF_Justin.pdf")
 
 if __name__=="__main__":
     app.run(debug=True)
